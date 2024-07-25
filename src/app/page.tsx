55985603cc042591b106d216d1cfdd5739e15e23
Debsourya Datta <91617309+debsouryadatta@users.noreply.github.com>
2024-07-25 16:31:18 +0530

    README.md updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,28 @@
-"use client";
+import { Gallery } from "@/components/gallery/Gallery";
+import { prisma } from "@/lib/db";
 
-import { Button } from "@/components/ui/button";
-import { signIn, signOut, useSession } from "next-auth/react";
-import Image from "next/image";
-import { useEffect } from "react";
-
-export default function Home() {
-  const session = useSession();
-
-  const fetchFunc = async () => {
-    const result = await fetch("/api/user");
-    console.log(await result.json());
+async function getCourses(){
+  try {
+    const res = await prisma.course.findMany({
+      include: {
+        user: true,
+      }
+    });
+    return res;
+  } catch (error) {
+    console.log("Error: ", error);
   }
 
-  useEffect(() => {
-    fetchFunc();
-  }, [])
+}
+
+export default async function page() {
+  const courses = await getCourses();
+  console.log("Courses: ", courses);
   
-
-
-  console.log(session);
-
   return (
-    <div className="dark">
-      <Button type="submit" onClick={() => signIn()}>
-        Sign In
-      </Button>
-      <Button type="submit" onClick={() => signOut({ callbackUrl: "/" })}>
-        Sign Out
-      </Button>
-      <Image
-        src={session?.data?.user?.image || "/default-avatar.png"}
-        width="40"
-        height="40"
-        alt="user avatar"
-        className="rounded-full"
-      />
-      <div>{session?.data?.user?.name}</div>
-
+    <div className="mt-20 min-h-[80vh]">
+      {courses?.length === 0 && <h1 className="mt-52 text-center text-2xl font-bold">No Courses Available!</h1>}
+      <Gallery courses={courses} />
     </div>
-  );
+  )
 }
