1693a149881806a5c09c21b6e8b51e8eef894015
Debsourya Datta <91617309+debsouryadatta@users.noreply.github.com>
2024-07-26 09:43:38 +0530

    auth testing 3

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,6 +12,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       session.user.id = user.id;
       return session;
     },
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    },
   },
   providers: [Google],
 });
