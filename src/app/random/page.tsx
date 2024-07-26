c54836e5af92902c4fe58ba40a08b1bd733cb60e
Debsourya Datta <91617309+debsouryadatta@users.noreply.github.com>
2024-07-26 08:50:51 +0530

    testing

---
+++ src/app/random/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from '@/components/common/sign-in'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        page
+        <SignIn />
+    </div>
+  )
+}
