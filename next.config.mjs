ccf7231f1db012e39585036efe4ba30e993ae613
Debsourya Datta <91617309+debsouryadatta@users.noreply.github.com>
2024-08-18 00:50:41 +0530

    changes for deployment 1

--- next.config.mjs
+++ next.config.mjs
@@ -42,6 +42,14 @@ const nextConfig = {
           },
         ],
       },
+
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    output: 'standalone',
 };
 
 export default nextConfig;
